5bc6fe7d08324bb2b6ffecf6bf721102d4f0f6f0
irinaka.robodone <irinaka.robodone@gmail.com>
2023-12-23 19:11:31 +0900

    add timer

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import random
 class SnakeGame:
     def __init__(self):
         # Pyxelウィンドウの初期化（160x120のサイズで）
-        pyxel.init(50, 50)
+        pyxel.init(40, 40, fps=60)
         # ゲームの初期状態を設定
+        self.speed = 3 # 蛇の速度
+        
         self.reset_game()
         # Pyxelのアップデート（ロジック）とドロー（描画）メソッドを設定
         pyxel.run(self.update, self.draw)
@@ -23,18 +25,28 @@ class SnakeGame:
         self.score = 0
         self.game_over = False
         # ゲームのタイマーを設定（30秒）
-        self.timer = 30
+        self.timer = 60
+        self.frame_counter = 0
+
 
     def spawn_food(self):
         # 食べ物をランダムな位置に生成
         while True:
-            food = (random.randint(0, 49), random.randint(0, 49))
+            food = (random.randint(0, 39), random.randint(0, 39))
             # ヘビの体と重ならない位置に食べ物を配置
             if food not in self.snake:
                 return food
 
     def update(self):
-        self.update_snake()
+        self.frame_counter += 1
+
+        # 一定のフレーム間隔でヘビを更新
+        if self.frame_counter % self.speed == 0:
+            self.update_snake()
+        
+        if self.frame_counter % 60 == 0:
+            self.timer -= 1
+
         # ゲームオーバー時の処理
         if self.game_over:
             # Rキーでゲームをリセット
@@ -45,7 +57,7 @@ class SnakeGame:
         # ヘビの頭が食べ物に触れた場合の処理
         if self.snake[0] == self.food:
             self.score += 1
-            self.timer += 1
+            self.timer += 10
             # ヘビの長さを増やす
             self.snake.append(self.snake[-1])
             # 新しい食べ物を生成
@@ -67,10 +79,10 @@ class SnakeGame:
         self.snake = [new_head] + self.snake[:-1]
 
         # キーボード入力によるヘビの方向の変更
-        if pyxel.btnp(pyxel.KEY_LEFT): self.direction = (-1, 0)
-        if pyxel.btnp(pyxel.KEY_RIGHT): self.direction = (1, 0)
-        if pyxel.btnp(pyxel.KEY_UP): self.direction = (0, -1)
-        if pyxel.btnp(pyxel.KEY_DOWN): self.direction = (0, 1)
+        if pyxel.btn(pyxel.KEY_LEFT): self.direction = (-1, 0)
+        if pyxel.btn(pyxel.KEY_RIGHT): self.direction = (1, 0)
+        if pyxel.btn(pyxel.KEY_UP): self.direction = (0, -1)
+        if pyxel.btn(pyxel.KEY_DOWN): self.direction = (0, 1)
 
     def draw(self):
         # 画面をクリア
